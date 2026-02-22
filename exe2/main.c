f1d93a55f5491a246e5a4f6e684b2421efdaf56c
Dovlask <laskdov@gmail.com>
2026-02-22 15:21:13 -0300

    Exercicio 2

--- exe2/main.c
+++ exe2/main.c
@@ -3,6 +3,7 @@
 #include <stdio.h>
 
 const int BTN_PIN = 26;
+const int BTN_PIN_2 = 7;
 
 int main() {
   stdio_init_all();
@@ -10,6 +11,9 @@ int main() {
   gpio_init(BTN_PIN);
   gpio_set_dir(BTN_PIN, GPIO_IN);
   gpio_pull_up(BTN_PIN);
+  gpio_init(BTN_PIN_2);
+  gpio_set_dir(BTN_PIN_2, GPIO_IN);
+  gpio_pull_up(BTN_PIN_2);
 
   while (true) {
     if (!gpio_get(BTN_PIN)) {
@@ -17,5 +21,10 @@ int main() {
       while (!gpio_get(BTN_PIN)) {
       };
     }
+      if (!gpio_get(BTN_PIN_2)) {
+        printf("Botao 2\n");
+        while (!gpio_get(BTN_PIN_2)) {
+        };
+      }
   }
 }
