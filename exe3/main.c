c813154bbf0350f1b70d9f71676be06a114bc566
Dovlask <laskdov@gmail.com>
2026-02-22 15:40:51 -0300

    Exercicio 3

--- exe3/main.c
+++ exe3/main.c
@@ -2,9 +2,65 @@
 #include "pico/stdlib.h"
 #include <stdio.h>
 
-int main() {
-  stdio_init_all();
+const int LED_PIN_R = 4;
+const int LED_PIN_G = 6;
+const int BTN_PIN_R = 28;
+const int BTN_PIN_G = 26;
 
-  while (true) {
-  }
+int main() {
+    stdio_init_all();
+
+    gpio_init(LED_PIN_R);
+    gpio_set_dir(LED_PIN_R, GPIO_OUT);
+    
+    gpio_init(LED_PIN_G);
+    gpio_set_dir(LED_PIN_G, GPIO_OUT);
+
+    gpio_init(BTN_PIN_R);
+    gpio_set_dir(BTN_PIN_R, GPIO_IN);
+    gpio_pull_up(BTN_PIN_R);
+
+    gpio_init(BTN_PIN_G);
+    gpio_set_dir(BTN_PIN_G, GPIO_IN);
+    gpio_pull_up(BTN_PIN_G);
+
+    int contador_r = 0;
+    int contador_g = 0;
+
+    while (true) {
+        
+        if (!gpio_get(BTN_PIN_R)) {
+            
+            if (contador_r == 0) {
+                gpio_put(LED_PIN_R, 1); 
+                contador_r = 1;         
+                
+                while (!gpio_get(BTN_PIN_R)) {}
+            } 
+            else if (contador_r == 1) {
+                gpio_put(LED_PIN_R, 0); 
+                contador_r = 0;      
+                
+                while (!gpio_get(BTN_PIN_R)) {}
+            }
+        }
+
+        if (!gpio_get(BTN_PIN_G)) {
+            
+            if (contador_g == 0) {
+                gpio_put(LED_PIN_G, 1); 
+                contador_g = 1;        
+                
+                while (!gpio_get(BTN_PIN_G)) {}
+            } 
+            else if (contador_g == 1) {
+                gpio_put(LED_PIN_G, 0); 
+                contador_g = 0;         
+                
+                
+                while (!gpio_get(BTN_PIN_G)) {}
+            }
+        }
+
+    }
 }
